e4147bb2b4039fcbf126ccde29588cb37961adec
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-03-09 16:16:14 -0300

    Group 21 - Adiciona requisito 2

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+  render () {
+    return (
+      <div>
+        <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
