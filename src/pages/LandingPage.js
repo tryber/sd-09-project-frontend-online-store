68a6d050f9bda929974da054de1bd778a9d31add
Luiz Vicentin <luizpavanello@gmail.com>
2021-03-09 17:07:04 -0300

    Feat Requisito 2.Crie uma página de listagem de produtos vazia

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
