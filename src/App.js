aa92db341efb0bc0d1c31e825433a610e463f83d
otacisos <otacisods@gmail.com>
2021-03-11 14:36:55 -0300

    Update Requisitos 4-5-6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+
 import Carrinho from './components/Carrinho';
 
 function App() {
