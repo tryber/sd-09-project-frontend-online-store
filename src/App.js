c70b15d2b4071e2f741cfc19755a48ecc7f83195
otacisos <otacisods@gmail.com>
2021-03-12 07:47:50 -0300

    Revert "Update Requisitos 4-5-6"
    This reverts commit aa92db341efb0bc0d1c31e825433a610e463f83d.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-
 import Carrinho from './components/Carrinho';
 
 function App() {
